c1bbaa85ca6ee9e4444f9b03f0e71968a8611f2e
Anders Lundström <anders@urkraft.se>
2016-09-03 12:58:37 +0200

    html

--- src/NotFound.purs
+++ src/NotFound.purs
@@ -5,4 +5,4 @@ import Pux.Html (Html, (#), div, h2, text)
 view :: forall state action. state -> Html action
 view state =
   div # do
-    h2 # text "404 Not Found"
+    h2 # text "404 Sidan kunde tyvärr inte hittas"
